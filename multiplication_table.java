287be2b752e24dc20ba8a986fe4808577f02ea07
Rohit Singh <105108296+ANPC1489Rohit@users.noreply.github.com>
2022-06-02 18:41:11 +0530

    Assignment_3_june_02

---
+++ multiplication_table.java
@@ -0,0 +1,33 @@
+
+public class multiplication_table 
+{
+
+	 public static void main(String[] args) {
+
+	        int num = 24;
+	        int num1 = 50;
+	        int num2 = 29;
+	        
+	        System.out.println("Multiplication of 24:");
+	        for(int i = 1; i <= 10; ++i)
+	        {
+	            System.out.printf("%d * %d = %d \n", num, i, num * i);
+	        }
+	        
+	        
+	        System.out.println("Multiplication of 50:");
+	        for(int i = 1; i <= 10; ++i)
+	        {
+	            System.out.printf("%d * %d = %d \n", num1, i, num1 * i);
+	        }
+	       
+	        
+	        System.out.println("Multiplication of 29:");
+	        for(int i = 1; i <= 10; ++i)
+	        {
+	            System.out.printf("%d * %d = %d \n", num2, i, num2 * i);
+	        }
+	        
+	    }
+	
+}
